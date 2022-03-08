25ab0ced5234194c0dea64654573608279eb55cd
shashwat0802 <shashwatgupta4656@gmail.com>
2022-03-08 22:10:42 +0530

    request airdrop and show wallet balance again

--- index.js
+++ index.js
@@ -15,13 +15,28 @@ const getWalletBalance = async () => {
   try {
     const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
     const walletBalance = await connection.getBalance(publickKey);
-    console.log(`Wallet balance is ${walletBalance}`);
+    console.log(`Wallet balance is ${walletBalance / LAMPORTS_PER_SOL}`);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const airDropSol = async () => {
+  try {
+    const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
+    const fromAirDropSignature = await connection.requestAirdrop(
+      publickKey,
+      2 * LAMPORTS_PER_SOL
+    );
+    await connection.confirmTransaction(fromAirDropSignature);
   } catch (error) {
     console.error(error);
   }
 };
 
 const main = async () => {
+  await getWalletBalance();
+  await airDropSol();
   await getWalletBalance();
 };
 
