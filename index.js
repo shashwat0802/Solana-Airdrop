a8a674d377387acc60964a820adc2d30b9fb0592
shashwat0802 <shashwatgupta4656@gmail.com>
2022-03-08 22:07:02 +0530

    get wallet balance

--- index.js
+++ index.js
@@ -10,3 +10,19 @@ const wallet = new Keypair();
 
 const publickKey = new PublicKey(wallet._keypair.publicKey);
 const secretKey = wallet._keypair.secretKey;
+
+const getWalletBalance = async () => {
+  try {
+    const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
+    const walletBalance = await connection.getBalance(publickKey);
+    console.log(`Wallet balance is ${walletBalance}`);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const main = async () => {
+  await getWalletBalance();
+};
+
+main();
